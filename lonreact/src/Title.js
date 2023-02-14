c5dae4df06ba209ac7deaf1f7e9ff8e7d326fcf4
jcduff00 <johncduffman@gmail.com>
2023-02-14 00:01:21 -0500

    header component added

---
+++ lonreact/src/Title.js
@@ -0,0 +1,15 @@
+import './Title.css';
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>LOOPZOOP</h1>
+			<div className="Title-Subtitle">The loopiest AND the zoopiest</div>
+    </div>
+  )
+}
+
+export default Title
