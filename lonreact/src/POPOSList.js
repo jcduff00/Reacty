38cf9dbe5783594215eb1528b5683279063e6660
jcduff00 <johncduffman@gmail.com>
2023-02-14 19:06:19 -0500

    refined dynamic content, fixed footer, added hours prop

--- lonreact/src/POPOSList.js
+++ lonreact/src/POPOSList.js
@@ -1,41 +1,62 @@
+import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
+import data from './sfpopos-data.json'
 
 function POPOSList() {
+  
+const spaces = data.map(( { title, address, images, hours } ) => {
+  return (
+    <POPOSSpace
+      key={title}
+      name={title}
+      address={address}
+      image={images[0]}
+      hours={hours}
+    />
+  )
+})
     return (
       <div className="POPOSList">
-        <POPOSSpace
-          name="Ron DeSantis"
-          address="46th Governor of Florida"
-          image="Ron_DeSantis_official_photo.jpg"
-        />
-        <POPOSSpace
-          name="Nikki Haley"
-          address="29th United Nations Ambassador"
-          image="Nikki_Haley_official_photo.jpg"
-        />
-        <POPOSSpace 
-          name="Mike Pence" 
-          address="48th Vice President of the United States" 
-          image="Mike_Pence_official_photo.jpg"
-          />
-        <POPOSSpace 
-          name="Tim Scott" 
-          address="Senator from South Carolina" 
-          image="Tim_Scott,_official_photo.jpg" 
-          />
-        <POPOSSpace 
-          name="Mike Pompeo" 
-          address="70th Secretary of State" 
-          image="Mike_Pompeo_official_photo.jpg"
-          />
-        <POPOSSpace 
-          name="Kristi Noem" 
-          address="33rd Governor of South Dakota" 
-          image="Kristi_Noem_official_photo.jpg" 
-          />
+        { spaces }
       </div>
     )
-  }
-  
-  export default POPOSList
+}
+//  // return (
+//       <div className="POPOSList">
+//         <POPOSSpace
+//           name="Ron DeSantis"
+//           address="46th Governor of Florida"
+//           image="Ron_DeSantis_official_photo.jpg"
+//         />
+//         <POPOSSpace
+//           name="Nikki Haley"
+//           address="29th United Nations Ambassador"
+//           image="Nikki_Haley_official_photo.jpg"
+//         />
+//         <POPOSSpace 
+//           name="Mike Pence" 
+//           address="48th Vice President of the United States" 
+//           image="Mike_Pence_official_photo.jpg"
+//           />
+//         <POPOSSpace 
+//           name="Tim Scott" 
+//           address="Senator from South Carolina" 
+//           image="Tim_Scott,_official_photo.jpg" 
+//           />
+//         <POPOSSpace 
+//           name="Mike Pompeo" 
+//           address="70th Secretary of State" 
+//           image="Mike_Pompeo_official_photo.jpg"
+//           />
+//         <POPOSSpace 
+//           name="Kristi Noem" 
+//           address="33rd Governor of South Dakota" 
+//           image="Kristi_Noem_official_photo.jpg" 
+//           />
+//       </div>
+//    )
+// }/
+
+export default POPOSList
+
--- lonreact/src/POPOSSpace.js
+++ lonreact/src/POPOSSpace.js
@@ -2,17 +2,18 @@ import logo from './logo.svg'
 import './POPOSSpace.css'
 
 function POPOSSpace(props) {
-  const { name, image, address } = props
+  const { name, image, address, hours } = props
   return (
     <div className='POPOSSpace'> 
+      <h1>{name}</h1>
       <img
         src={`${process.env.PUBLIC_URL}/images/${image}`}
         width="100"
         height="100"
         alt="Hello"
       />
-      <h1>{name}</h1>
       <div>{address}</div>
+      <div>{hours}</div>
     </div>
   )
 }
